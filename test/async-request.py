de6333307c0e431b78a71848e19460629e20aa3f
Ilya Antonov <ilya.antonov@dreams-ai.com>
2020-03-01 15:42:43 +0800

    configuring logstash filter

--- test/async-request.py
+++ test/async-request.py
@@ -1,18 +1,29 @@
+import requests
 import logging
 from logstash_async.handler import AsynchronousLogstashHandler
+from logstash_async.formatter import LogstashFormatter
 
 logger = logging.getLogger('python-logstash-logger')
 logger.setLevel(logging.INFO)
-logger.addHandler(AsynchronousLogstashHandler(
-    'elkdreams-ai.com', 8080, database_path=None))
+
+
+lg_formatter = LogstashFormatter(
+    message_type='execution_analysis',
+    extra_prefix='executiondoc')
+lg_handler = AsynchronousLogstashHandler(
+    'elkdreams-ai.com', 8080, database_path=None)
+lg_handler.setFormatter(lg_formatter)
+
+logger.addHandler(lg_handler)
+
 
 extra = {
-    'message_type': 'execution_analysis',
-    'Bank': 'ICBC',
-    'Function Name': 'Yelp',
-    'Success': True,
-    'Parent List': ['test1', 'test2', 'test3'],
-    'Account': 'bb56893',
-    'End Point': True
+    'bank': 'ICBC',
+    'function': 'Yelp',
+    'trigger_function': True,
+    'ancestor_functions': ['test1', 'test2', 'test3'],
+    'success': True,
+    'account': 'bb56893',
+    'clientip': requests.get('http://ip.42.pl/raw').text
 }
-logger.info('python-logstash: test extra fields', extra=extra)
+logger.info("Report on failed function", extra=extra)
